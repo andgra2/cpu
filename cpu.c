69eb6231a63037f5fd89592f078028e1cfa8328c
Per-Olov Jernberg <possan@possan.se>
2014-03-19 22:34:59 +0100

    cpu.c kompilerar

--- cpu.c
+++ cpu.c
@@ -12,6 +12,13 @@ enum instr_num {
 	instr_num_copy
 };
 
+static enum instr_arg_type iat_get(struct cpu *cpu, cell ian)
+{
+	cell c = memory_get(cpu->m, cpu->pc_ni);
+	return (enum instr_arg_type) (c >> (16 + 4 * ian)) & 0x0000000F;
+}
+
+
 static cell arg_im_get(struct cpu *cpu, cell ian)
 {
 	return memory_get(cpu->m, cpu->pc_ni + ian + 1);
@@ -41,12 +48,6 @@ static void arg_mem2_set(struct cpu *cpu, cell ian, cell c)
 	memory_set(cpu->m, pc, c);
 }
 
-static instr_arg_type iat_get(struct cpu *cpu, cell ian)
-{
-	cell c = memory_get(cpu->m, cpu->pc_ni);
-	return (instr_arg_type) (c >> (16 + 4 * ian)) & 0x0000000F;
-}
-
 static cell arg_get(struct cpu *cpu, cell ian)
 {
 	switch (iat_get(cpu, ian))
@@ -73,6 +74,35 @@ static void arg_set(struct cpu *cpu, cell ian, cell c)
 	}
 }
 
+static void instr_terminate(struct cpu *cpu)
+{
+	cpu->pc_ni = pc_null;
+}
+
+static void instr_jump(struct cpu *cpu)
+{
+	cpu->pc_ni = arg_get(cpu, 0);
+}
+
+static void instr_jump_cond(struct cpu *cpu)
+{
+	if (arg_get(cpu, 0))
+		cpu->pc_ni = arg_get(cpu, 1);
+	cpu->pc_ni += 3;
+}
+
+static void instr_copy(struct cpu *cpu)
+{
+	arg_set(cpu, 1, arg_get(cpu, 0));
+	cpu->pc_ni += 3;
+}
+
+static void instr_add(struct cpu *cpu)
+{
+	arg_set(cpu, 2, arg_get(cpu, 0) + arg_get(cpu, 1));
+	cpu->pc_ni += 4;
+}
+
 static cell in_get(struct cpu *cpu)
 {
 	return memory_get(cpu->m, cpu->pc_ni) & 0x0000FFFF;
@@ -87,7 +117,7 @@ extern void cpu_create(struct cpu *cpu, struct memory *m)
 extern void cpu_run_init(struct cpu *cpu, cell pc_ni)
 {
 	cpu->pc_ni = pc_ni;
-} 
+}
 
 extern bool cpu_run_step(struct cpu *cpu)
 {
@@ -111,34 +141,3 @@ extern bool cpu_is_run(struct cpu *cpu)
 {
 	return cpu->pc_ni != pc_null;
 }
-
-void instr_terminate(struct cpu *cpu)
-{
-	cpu->pc_ni = pc_null;
-}
-
-void instr_jump(struct cpu *cpu)
-{
-	cpu->pc_ni = arg_get(cpu, 0);
-}
-
-void instr_jump_cond(struct cpu *cpu)
-{
-	if (arg_get(cpu, 0))
-		cpu->pc_ni = arg_get(cpu, 1);
-	cpu->pc_ni += 3;
-}
-
-void instr_copy(struct cpu *cpu)
-{
-	arg_set(cpu, 1, arg_get(cpu, 0));
-	cpu->pc_ni += 3;
-}
-
-void instr_add(struct cpu *cpu)
-{
-	arg_set(cpu, 2, arg_get(cpu, 0) + arg_get(cpu, 1));
-	cpu->pc_ni += 4;
-}
-
-
