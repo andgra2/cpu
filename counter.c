77fc7327bdc02e84fab36d1fdb7c5700dfd484f5
Per-Olov Jernberg <possan@possan.se>
2014-03-20 00:04:26 +0100

    assembler macros

--- counter.c
+++ counter.c
@@ -1,124 +1,9 @@
 #include "cpu.h"
 #include "memory.h"
-
-cell make_instruction(enum instr_num instr,
-		enum instr_arg_type arg0,
-		enum instr_arg_type arg1,
-		enum instr_arg_type arg2,
-		enum instr_arg_type arg3) {
-	return (arg3 << 28) + (arg2 << 24) + (arg1 << 20) + (arg0 << 16) + instr;
-}
-
-#define ADD_IIM(in1, in2, out) \
-	{\
-		cell instr = make_instruction(instr_num_add, im, im, mem, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, in1); writepointer ++;\
-		memory_set(&memory, code+writepointer, in2); writepointer ++;\
-		memory_set(&memory, code+writepointer, out); writepointer ++;\
-	}
-
-#define ADD_IMM(in1, in2, out) \
-	{\
-		cell instr = make_instruction(instr_num_add, im, mem, mem, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, in1); writepointer ++;\
-		memory_set(&memory, code+writepointer, in2); writepointer ++;\
-		memory_set(&memory, code+writepointer, out); writepointer ++;\
-	}
-
-#define JUMP_I(address) \
-	{\
-		cell instr = make_instruction(instr_num_jump, im, 0, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, address); writepointer ++;\
-	}
-
-#define JUMP_M(addressptr) \
-	{\
-		cell instr = make_instruction(instr_num_jump, mem, 0, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, addressptr); writepointer ++;\
-	}
-
-#define JUMP_COND_MI(valueaddressptr, jumpaddress) \
-	{\
-		cell instr = make_instruction(instr_num_jump_cond, mem, im, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, valueaddressptr); writepointer ++;\
-		memory_set(&memory, code+writepointer, jumpaddress); writepointer ++;\
-	}
-
-#define COPY_MM(valueptr, addressptr) \
-	{\
-		cell instr = make_instruction(instr_num_copy, mem, mem, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, valueptr); writepointer ++;\
-		memory_set(&memory, code+writepointer, addressptr); writepointer ++;\
-	}
-
-#define COPY_IM(value, addressptr) \
-	{\
-		cell instr = make_instruction(instr_num_copy, im, mem, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, value); writepointer ++;\
-		memory_set(&memory, code+writepointer, addressptr); writepointer ++;\
-	}
-
-#define AND_IMM(value, addressptr, address2ptr) \
-	{\
-		cell instr = make_instruction(instr_num_and, im, mem, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, value); writepointer ++;\
-		memory_set(&memory, code+writepointer, addressptr); writepointer ++;\
-		memory_set(&memory, code+writepointer, address2ptr); writepointer ++;\
-	}
-
-#define OR_IMM(value, addressptr, address2ptr) \
-	{\
-		cell instr = make_instruction(instr_num_or, im, mem, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, value); writepointer ++;\
-		memory_set(&memory, code+writepointer, addressptr); writepointer ++;\
-		memory_set(&memory, code+writepointer, address2ptr); writepointer ++;\
-	}
-
-#define JUMP_COND_MM(valueaddressptr, jumpaddressptr) \
-	{\
-		cell instr = make_instruction(instr_num_jump_cond, mem, mem, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, valueaddressptr); writepointer ++;\
-		memory_set(&memory, code+writepointer, jumpaddressptr); writepointer ++;\
-	}
-
-#define PRINT_I(address) \
-	{\
-		cell instr = make_instruction(instr_num_print, im, 0, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, address); writepointer ++;\
-	}
-
-#define PRINT_M(addressptr) \
-	{\
-		cell instr = make_instruction(instr_num_print, mem, 0, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-		memory_set(&memory, code+writepointer, addressptr); writepointer ++;\
-	}
-
-#define TERMINATE() \
-	{\
-		cell instr = make_instruction(instr_num_terminate, 0, 0, 0, 0);\
-		memory_set(&memory, code+writepointer, instr); writepointer ++;\
-	}
+#include "asm.h"
 
 int main() {
-	struct memory memory;
-	memory_create(&memory);
-	cell code, code2;
-	code = memory_new_block(&memory);
-	int writepointer = 0;
-
-
+	ASM_INIT();
 
 	// 0 1 2
 	COPY_IM('0', code+4);
@@ -135,20 +20,5 @@ int main() {
 	// 14
 	TERMINATE();
 
-
-
-	struct cpu cpu;
-	cpu_create(&cpu, &memory);
-	cpu_run_init(&cpu, code+0);
-	while(cpu_run_step(&cpu)) {
-#ifdef APA
-		printf("instruktionspekare @ %05X ", cpu.pc_ni-code);
-		for(int i=0; i<20; i++) {
-			printf(" %02X", memory_get(&memory, code+i));
-		}
-		printf("\n");
-#endif
-		usleep(10000);
-	}
-	memory_destroy(&memory);
+	ASM_RUN()
 }
