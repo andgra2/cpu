562fb1b3b505ce9d666fa566ae589aa059323129
ansgrd2 <anders.granlund.0@gmail.com>
2014-03-30 18:11:14 +0200

    Implements utility functionality.

---
+++ util__test.c
@@ -0,0 +1,26 @@
+#include "util.h"
+
+int main()
+{
+	int ret;
+
+	char s[] = "Hello!";
+	char *sc;
+	if ((sc = str_clone(s)) == NULL) { ret = -1; goto out_1; }
+	printf("%s\n", sc);
+
+	printf("\n");
+
+	s[0] = 'h';
+	sc[2] = 'L';
+	printf("%s %s", s, sc);
+
+	printf("\n");
+
+	free(sc);
+
+	ret = 0;
+out_1:
+	return ret;
+}
+
